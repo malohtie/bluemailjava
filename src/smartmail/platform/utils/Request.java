2afd71f243a1a6a4110a66eab0bdc8357e3d225d
Amine Bureau <malohtie@gmail.com>
2020-06-19 00:08:05 +0100

    Fixs

--- src/smartmail/platform/utils/Request.java
+++ src/smartmail/platform/utils/Request.java
@@ -8,24 +8,25 @@ import java.net.URL;
 
 public class Request {
     public static String[] downloadFile(String link, String fileName) throws Exception {
-        URL url = new URL(link);
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setReadTimeout(120000);
-        connection.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
-        connection.addRequestProperty("User-Agent", "Mozilla");
-        connection.addRequestProperty("Referer", "google.com");
-        boolean redirect = false;
-        int status = connection.getResponseCode();
-        if ((status != 200 && status == 302) || status == 301 || status == 303)
-            redirect = true;
-        if (redirect) {
-            String newUrl = connection.getHeaderField("Location");
-            connection = (HttpURLConnection)(new URL(newUrl)).openConnection();
+        String location = link;
+        HttpURLConnection connection = null;
+        URL url = null;
+        int times = 0;
+
+        for (;;) {
+            if(times > 10) {
+                throw new Exception("To mush redirects");
+            }
+            url = new URL(location);
+            connection = (HttpURLConnection)url.openConnection();
             connection.setReadTimeout(120000);
-            connection.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
-            connection.addRequestProperty("User-Agent", "Mozilla");
-            connection.addRequestProperty("Referer", "google.com");
+            connection.setInstanceFollowRedirects(true);
+            String redirectLocation = connection.getHeaderField("Location");
+            if (redirectLocation == null) break;
+            location = redirectLocation;
+            times ++ ;
         }
+
         String type = connection.getContentType();
         long totalDataRead = 0L;
         if (type == null)
